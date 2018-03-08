42434896099c789115c3afadaaa5231989ce7849
Priyabrata Biswal <pbiswal.3d@gmail.com>
2018-03-08 01:32:34 +0100

    Setup Menu Functionalities Initialize Tank Player Controller Class

---
+++ BattleTanks/Source/BattleTanks/TankPlayerController.cpp
@@ -0,0 +1,7 @@
+// Copyright 2018 Priyabrata Biswal. All rights reserved.
+
+#include "TankPlayerController.h"
+
+
+
+
---
+++ BattleTanks/Source/BattleTanks/TankPlayerController.h
@@ -0,0 +1,20 @@
+// Copyright 2018 Priyabrata Biswal. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKS_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
